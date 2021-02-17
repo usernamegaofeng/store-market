dcd3a1ced4574deaa6b622e35d3f78dfe71d3dc9
gaofeng <944742829@qq.com>
2021-02-17 16:28:33 +0800

    提交代码

---
+++ store-market-auth/src/main/java/pers/store/market/auth/AuthServiceApplication.java
@@ -0,0 +1,20 @@
+package pers.store.market.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author Gaofeng
+ * @date 2021/2/17 下午2:26
+ * @description: 认证服务启动类
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class AuthServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+}
---
+++ store-market-auth/src/main/java/pers/store/market/auth/config/ViewsControllerConfig.java
@@ -0,0 +1,26 @@
+package pers.store.market.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Gaofeng
+ * @date 2021/2/17 下午3:08
+ * @description: 视图页面跳转配置
+ * 不需要写controller进行页面的跳转
+ */
+@Configuration
+public class ViewsControllerConfig implements WebMvcConfigurer {
+
+    /**
+     * 配置路径和页面映射
+     *
+     * @param registry 视图注册
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/register.html").setViewName("register");
+    }
+}
