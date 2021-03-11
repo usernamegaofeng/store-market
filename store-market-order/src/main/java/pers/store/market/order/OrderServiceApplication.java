20b0131fed5e901c19a46b0a75489d5f99d4defa
gaofeng <944742829@qq.com>
2021-03-11 23:26:28 +0800

    提交代码

--- store-market-order/src/main/java/pers/store/market/order/OrderServiceApplication.java
+++ store-market-order/src/main/java/pers/store/market/order/OrderServiceApplication.java
@@ -1,6 +1,5 @@
 package pers.store.market.order;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
